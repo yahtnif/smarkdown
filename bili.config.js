bbd22ac3d33913ac89efa28f63ee57d8105f8bce
Yahtnif <yahtnif@gmail.com>
2018-06-25 09:57:00 +0800

    Setting sizeLimit in bili

--- bili.config.js
+++ bili.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  banner: true,
   input: 'src/index.ts',
-  format: ['es', 'umd', 'umd-min']
+  format: ['es', 'umd', 'umd-min'],
+  sizeLimit: {
+    'es': '14KB',
+    'umd': '15KB',
+    'umd-min': '9KB'
+  }
 }

dc35024beafa65f7e8fd2b62389651908b94cfff
Yahtnif <yahtnif@gmail.com>
2018-08-24 17:54:43 +0800

    Update bili:sizeLimit

--- bili.config.js
+++ bili.config.js
@@ -3,8 +3,8 @@ module.exports = {
   input: 'src/index.ts',
   format: ['es', 'umd', 'umd-min'],
   sizeLimit: {
-    'es': '14KB',
-    'umd': '15KB',
-    'umd-min': '9KB'
+    'es': '15KB',
+    'umd': '16KB',
+    'umd-min': '10KB'
   }
 }

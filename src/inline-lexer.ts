846b3946ed9649ae19242b3f8acff30efbf0300b
Yahtnif <yahtnif@gmail.com>
2018-08-30 11:57:24 +0800

    Revert: keep strikethrough text wrapped in two tildes for GFM

--- src/inline-lexer.ts
+++ src/inline-lexer.ts
@@ -170,7 +170,13 @@ export class InlineLexer {
 
     const _backpedal = /(?:[^?!.,:;*_~()&]+|\([^)]*\)|&(?![a-zA-Z0-9]+;$)|[?!.,:;*_~)]+(?!$))+/
 
-    const del = /^~+(?=\S)([\s\S]*?\S)~+/
+    /**
+     * [GFM Strikethrough](https://github.github.com/gfm/#strikethrough-extension-)
+     * Strikethrough text is any text wrapped in two tildes (~).
+     * For now, gfm allow strikethrough text wrapped in single tilde on github, it's conflict with subscript extension.
+     * [Single tilde in GFM spec](https://github.com/github/cmark/issues/99)
+     */
+    const del = /^~~(?=\S)([\s\S]*?\S)~~/
 
     const text = new ExtendRegexp(base.text)
       .setGroup(']|', '~]|')

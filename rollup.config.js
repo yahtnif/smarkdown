0264ed5013697cedb812f969b46625432d1f603d
Yahtnif <yahtnif@gmail.com>
2018-10-14 09:55:27 +0800

    Add banner

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,24 @@
 import { uglify } from 'rollup-plugin-uglify'
 import clear from 'rollup-plugin-clear'
 import filesize from 'rollup-plugin-filesize'
+import license from 'rollup-plugin-license'
 import typescript from 'rollup-plugin-typescript2'
-import pkg from './package.json';
+import pkg from './package.json'
+
+const banner = `/*!
+* Smarkdown v${pkg.version}
+* (c) 2018-present ${pkg.author}
+* Released under the ${pkg.license} License.
+*/`
 
 const input = 'src/index.ts'
-const sharedPlugins = [typescript()]
-const commonConfig = {
-  input,
-  plugins: sharedPlugins
-}
+const sharedPlugins = [
+  typescript(),
+  license({
+    banner
+  })
+]
+const commonConfig = { input }
 
 export default [
   {
@@ -20,10 +29,10 @@ export default [
       format: 'umd'
     },
     plugins: [
+      ...sharedPlugins,
       clear({
         targets: ['dist']
       }),
-      ...sharedPlugins,
       uglify(),
       filesize(),
     ]
@@ -33,6 +42,9 @@ export default [
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
+    ],
+    plugins: [
+      ...sharedPlugins
     ]
   }
 ]

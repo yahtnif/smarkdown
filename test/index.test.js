68ec03cca86c88a4e39916bccfb4c8da07547faf
Yahtnif <yahtnif@gmail.com>
2018-10-14 14:29:16 +0800

    Add more tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,39 @@
 const klawSync = require('klaw-sync')
 const fs = require('fs-extra')
 const { expect } = require('chai')
+const JSON5 = require('json5')
 const Smarkdown = require('../dist/smarkdown')
 
 const files = klawSync('test/tasks', { nodir: true })
 
 describe('Smarkdown', function() {
   for (const file of files) {
-    const data = fs.readFileSync(file.path, 'utf-8')
     const filename = file.path.split('/').slice(-1)[0]
-    const [text, html] = data.split(/\n{3}/)
+
+    const data = fs.readFileSync(file.path, 'utf-8')
+
+    let [options, text] = data.split('\n===\n')
+
+    if (text) {
+      options = options
+        .trim()
+        .split(/\n+/)
+        .map(s => s + ',')
+        .join('\n')
+      options = JSON5.parse(`{${options}}`)
+    } else {
+      text = options
+      options = null
+    }
+
+    let [actual, expected] = text.split(/\n{4,}/)
+
+    if (!expected.endsWith('\n')) {
+      expected += '\n'
+    }
 
     it(filename, function() {
-      expect(Smarkdown.parse(text)).to.equal(html)
+      expect(Smarkdown.parse(actual, options)).to.equal(expected)
     })
   }
 })
